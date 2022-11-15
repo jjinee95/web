759e61b67227c5fb7f0b37db1bd993f3467ab3b2
jjinee95 <117340104+jjinee95@users.noreply.github.com>
2022-11-15 23:42:08 +0900

    Add files via upload

--- object3.js
+++ object3.js
@@ -11,13 +11,31 @@ class Book {
 
 
 //  출판사 다산북스, 김영사, 범우사
-let b1 = new Book();
+let b1 = new Book('제목1', '저자1', '다산북스', 7000);
+let b2 = new Book('제목2', '저자2', '김영사', 9000);
+let b3 = new Book('제목3', '저자3', '범우사', 8000);
+let b4 = new Book('제목4', '저자4', '다산북스', 12000);
+let b5 = new Book('제목5', '저자5', '김영사', 11000);
+let b6 = new Book('제목6', '저자6', '범우사', 13000);
 
 
 let books = []; // 6건 입력
+books.push(b1);
+books.push(b2);
+books.push(b3);
+books.push(b4);
+books.push(b5);
+books.push(b6);
+
 
 function findBooks(press) {
 //같은값 있으면
+
+    for (let book of books ) {
+        if (book = press) {
+            return book
+        }
+    }
 }
 
 let result = findBooks("김영사");
